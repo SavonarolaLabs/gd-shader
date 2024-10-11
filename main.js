ad660973c481a48d1c05528c93537f8499728cb9
c8e4 <c8e4@proton.me>
2024-10-11 10:10:06 +0200

    initApp()

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log('gl hf');
+function initApp() {
+  const canvas = document.createElement('canvas');
+  const gl = canvas.getContext('webgl2');
+
+  if (!gl) {
+    throw new Error('WebGL2 not supported');
+  }
+
+  document.body.appendChild(canvas);
+
+  const app = {
+    canvas,
+    gl,
+    glSize: { w: 0, h: 0 },
+    dpr: Math.min(window.devicePixelRatio, 2),
+    updateSize() {
+      const w = window.innerWidth;
+      const h = window.innerHeight;
+      this.glSize = { w: w * this.dpr, h: h * this.dpr };
+
+      this.canvas.width = this.glSize.w;
+      this.canvas.height = this.glSize.h;
+      this.canvas.style.width = `${w}px`;
+      this.canvas.style.height = `${h}px`;
+
+      this.gl.viewport(0, 0, this.glSize.w, this.glSize.h);
+    },
+  };
+
+  app.updateSize();
+  window.addEventListener('resize', app.updateSize);
+  return app;
+}
+
+const app = initApp();
